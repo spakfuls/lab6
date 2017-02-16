2dd6159e05aac8b078cc9a96f0b66dc09c80fa00
Kaye Geronilla <kgeronil@ucsd.edu>
2017-02-16 10:46:35 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,21 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+
+	console.log("called " + idNumber);
+}
+
+function addProject(result){
+	console.log(result);
+
+	var data = "#project" + result['id'] + " .details";
+	var projectPic = '<img src="' + result['image'] + '"class="detailsImage">' 
+	+ '<p>' + result['title'] 
+	+ '</p>' + '<p><small>' + result['date'] + '</small></p>';
+
+	$(data).html(projectPic + result['summary']);
 }
 
 /*
@@ -35,4 +50,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", addColor);
+}
+
+function addColor(result){
+	console.log(result);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
